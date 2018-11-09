c4081268e87572f775710a3bd9816cba4a884749
TheDorkKnightRises <samriddhabasu@gmail.com>
2018-11-09 07:19:32 +0530

    Update app metadata and icon, add development service worker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -51,6 +51,16 @@ export function register(config) {
         registerValidSW(swUrl, config);
       }
     });
+} else if (process.env.NODE_ENV === 'development' && 'serviceWorker' in navigator) {
+      window.addEventListener('load', function() {
+        navigator.serviceWorker.register('./sw.js').then(function(registration) {
+          // Registration was successful
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+          // registration failed :(
+          console.log('ServiceWorker registration failed: ', err);
+        });
+      });
   }
 }
 
